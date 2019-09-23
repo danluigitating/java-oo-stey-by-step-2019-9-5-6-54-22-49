f519abf247baa3c3e0bd5198958b97ab1f56955f
Dan Luigi Tating <dan.luigi.tating@oocl.com>
2019-09-23 18:59:43 +0800

    Added answers for Activity 2

--- src/main/java/practice02/Person.java
+++ src/main/java/practice02/Person.java
@@ -1,4 +1,27 @@
 package practice02;
 
 public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+        super();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return "My name is " + name + ". I am " + age + " years old.";
+    }
 }
--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,29 @@
 package practice02;
 
-public class Student {
+public class Student extends Person {
+    String name;
+    int age;
+    int classNumber;
+
+    public Student(String name, int age, int classNumber) {
+        this.name = name;
+        this.age = age;
+        this.classNumber= classNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getKlass() {
+        return classNumber;
+    }
+
+    public String introduce() {
+        return "I am a Student. I am at Class " + classNumber + ".";
+    }
 }
