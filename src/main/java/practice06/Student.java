f9d4e676251232d11bd489c9c877de1cc76d4a93
Dan Luigi Tating <dan.luigi.tating@oocl.com>
2019-09-24 17:27:40 +0800

    Added answers for Practice 6

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,19 @@
 package practice06;
 
-public class Student {
+public class Student extends Person {
+    int classNumber;
+
+    public Student(String name, int age, int classNumber) {
+        super(name, age);
+        this.classNumber= classNumber;
+    }
+
+    public int getKlass() {
+        return classNumber;
+    }
+
+    @Override
+    public String introduce() {
+        return introduce2() + " I am a Student. I am at Class " + classNumber + ".";
+    }
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,26 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    int classNumber;
+
+    public Teacher(String name, int age, int classNumber) {
+        super(name,age);
+        this.classNumber= classNumber;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return classNumber;
+    }
+
+    @Override
+    public String introduce() {
+        if(classNumber!=0)
+            return introduce2() + " I am a Teacher. I teach Class " + classNumber + ".";
+        else
+            return introduce2() + " I am a Teacher. I teach No Class.";
+    }
 }
