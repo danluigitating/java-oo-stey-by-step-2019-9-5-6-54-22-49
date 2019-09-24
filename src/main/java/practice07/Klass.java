ce05592d5a550593eac9d6808862d6d7d2871c21
Dan Luigi Tating <dan.luigi.tating@oocl.com>
2019-09-24 18:42:41 +0800

    Added answers for Practice 7

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,18 @@
 package practice07;
 
 public class Klass {
+    int classNumber;
+
+    public Klass(int classNumber) {
+        this.classNumber = classNumber;
+    }
+
+    public Integer getNumber() {
+        return classNumber;
+    }
+
+    public String getDisplayName() {
+        return "Class " + classNumber;
+    }
+
 }
--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,27 @@
 package practice07;
 
-public class Student {
+public class Student extends Person {
+    Klass klass;
+
+    public Student(String name, int age, int classNumber) {
+        super(name, age);
+    }
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return introduce2() + " I am a Student. I am at Class " + klass.getNumber() + ".";
+    }
 }
