b221924ec9632a8b5c12f01b63fbfae60eb68966
Dan Luigi Tating <dan.luigi.tating@oocl.com>
2019-09-24 15:34:22 +0800

    Added answers to Practice 3

--- src/main/java/practice03/Person.java
+++ src/main/java/practice03/Person.java
@@ -1,4 +1,25 @@
 package practice03;
 
 public class Person {
+    String name;
+    int age;
+
+    public Person() {}
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return "My name is " + name + ". I am " + age + " years old.";
+    }
 }
