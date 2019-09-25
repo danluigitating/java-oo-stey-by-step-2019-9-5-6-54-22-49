be6b4b763f4f0c79ce38de600f0510541b60c07d
Dan Luigi Tating <dan.luigi.tating@oocl.com>
2019-09-25 16:04:06 +0800

    Added answers for Practice 10

--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,43 @@
 package practice10;
 
-public class Teacher {
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Teacher extends Person{
+    List<Klass> classes;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, List<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+        System.out.println(classes.size());
+    }
+
+    public List<Klass> getClasses() {
+        return classes;
+    }
+
+    public String introduce1(){
+        return String.format("%s I am a Teacher.", super.introduce());
+    }
+
+    public boolean isTeaching(Student student){
+        for (Klass klass : classes){
+            if (klass == student.getKlass()) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("%s I teach %s.", introduce1(), (classes == null ? "No Class" : "Class" + classes.stream().map(klass -> " " + klass.getNumber()).collect(Collectors.joining(","))));
+    }
+
+    public String introduceWith(Student student){
+        return String.format("%s I%steach %s.", introduce1(), (isTeaching(student) ? " " : " don't "), student.getName());
+    }
 }
+
