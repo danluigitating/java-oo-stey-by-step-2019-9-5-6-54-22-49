a424e642d200f68903673f6bdb0d661644e07a49
Dan Luigi Tating <dan.luigi.tating@oocl.com>
2019-09-25 11:12:43 +0800

    Added Answers for Practice 9

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,41 @@
 package practice09;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Klass {
+    int classNumber;
+    Student leaderName;
+    Set<Student> member;
+
+    public Klass(int classNumber) {
+        this.classNumber = classNumber;
+        member = new HashSet<>();
+    }
+
+    public Integer getNumber() {
+        return classNumber;
+    }
+
+    public void assignLeader(Student student) {
+        if (member.contains(student)) {
+            this.leaderName = student;
+        }
+        else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public void appendMember(Student student) {
+        member.add(student);
+    }
+
+
+    public Student getLeader() {
+        return leaderName;
+    }
+
+    public String getDisplayName() {
+        return "Class " + classNumber;
+    }
 }
